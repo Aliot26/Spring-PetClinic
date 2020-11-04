2583cadee4f7647cbc3ec5b0881e517378c24696
Aliot26 <olgakochanowa@gmail.com>
2020-11-04 13:09:47 +0100

    added endpoints for vets, changed security key

--- src/main/java/com/petclinic/spring/config/SecurityConfig.java
+++ src/main/java/com/petclinic/spring/config/SecurityConfig.java
@@ -23,9 +23,9 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
-    private static String[] PATH_USERS_ARR = new String[]{"/", "index", "index.html", "/login", "/vets/**", "/resources/**", "/images/**", "/api/vets"};
+    private static String[] PATH_USERS_ARR = new String[]{"/", "index", "index.html", "/authenticate", "/vets/**", "/resources/**", "/api/auth/signin", "/images/**", "/api/vets"};
     private static final String USER_ENDPOINT = "/owners/** ";
-    private static final String ADMIN_ENDPOINT = "/admin/**";
+    private static String[] ADMIN_ENDPOINT = new String[]{"/admin/**", "api/vets/**"};
     private static final String LOGIN_ENDPOINT = "/api/v1/auth/*";
 
     @Autowired
